e246e47fcc6ac87901141ac8bf8ecbf404ca56da
Pranav-Rustagi <pranav001100@gmail.com>
2023-08-25 15:10:49 +0530

    added command in help

--- bin/constants/index.js
+++ bin/constants/index.js
@@ -5,14 +5,14 @@ const usage = [{
     cmd: "[video/playlist] [options] <url>",
     desc: "Download a youtube video/playlist"
 }, {
+    cmd: "--version",
+    desc: "View version"
+}, {
     cmd: "--help",
     desc: "View help"
 }];
 
 const options = [{
-    option: "-h, --help",
-    desc: "View help"
-}, {
     option: "-i, --info",
     desc: "View details of a youtube video/playlist",
 }, {
@@ -21,6 +21,12 @@ const options = [{
 }, {
     option: "-ao, --audioonly",
     desc: "Download a youtube video/playlist as audio/s"
+}, {
+    option: "-v, --version",
+    desc: "View version"
+}, {
+    option: "-h, --help",
+    desc: "View help"
 }];
 
 module.exports = { usage, options };
